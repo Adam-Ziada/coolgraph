bd8f5cbfbd467ef97934f8df7ac2ca8394002616
Adam Ziada <adamsziada@gmail.com>
2016-12-08 08:47:16 -0800

    updated function tests for facet2D

--- tests/testthat/test-facet2D.R
+++ tests/testthat/test-facet2D.R
@@ -11,32 +11,24 @@ testthat::test_that("makes a 2D faceted graph", {
 
 	b <- ggplot2::ggplot(my_cars, ggplot2::aes(wt, mpg)) +
 		ggplot2::geom_point(alpha = 1) +
-		ggplot2::facet_wrap(c(paste("am"), paste("cyl"))) +
+		ggplot2::facet_wrap(c("am", "cyl")) +
 		ggplot2::labs(x = "am") +
 		ggplot2::labs(y = "cylinder") +
 		ggplot2::theme_bw(base_size = 10)
 
-facet2D <- function(data, c1, c2, f1, f2, xlab, ylab) {
-	ggplot2::ggplot(data = data, ggplot2::aes(c1, c2)) +
-		ggplot2::geom_point(alpha = 1) +
-		ggplot2::facet_wrap(c(paste(f1), paste(f2))) +
-		ggplot2::labs(x = xlab) +
-		ggplot2::labs(y = ylab) +
-		ggplot2::theme_bw(base_size = 10)
-}
 
 
-	c <- facet2D(my_cars, my_cars$wt, my_cars$mpg, "am", "cyl", "am", "cylinder")
+	c <- facet2D(my_cars, "wt", "mpg", "am", "cyl", "am", "cylinder")
 
 
 
-#	testthat::expect_silent(c)
-	#	testthat::expect_is(c, "ggplot")
-	#	testthat::expect_equal(b$data, c$data)
-	#	testthat::expect_equal(b$labels, c$labels)
-	#	testthat::expect_equal(b$coordinates, c$coordinates)
-	#	testthat::expect_equal(b$scales, c$scales)
-	#	testthat::expect_equal(b$layers, c$layers)
+	testthat::expect_silent(c)
+		testthat::expect_is(c, "ggplot")
+		testthat::expect_equal(b$data, c$data)
+		testthat::expect_equal(b$labels, c$labels)
+		testthat::expect_equal(b$coordinates, c$coordinates)
+		testthat::expect_equal(b$scales, c$scales)
+		testthat::expect_equal(b$layers, c$layers)
 
 
 
