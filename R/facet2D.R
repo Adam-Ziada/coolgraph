e1f9509fd73a3acb308dddd448e62bcf6a110b17
Adam Ziada <adamsziada@gmail.com>
2016-12-08 08:38:57 -0800

    asdfa

--- R/facet2D.R
+++ R/facet2D.R
@@ -31,21 +31,21 @@ facet2D <- function(data, c1, c2, f1, f2, xlab = "x-axis", ylab = "y-axis") {
 	if(!(is.factor(f2) | is.character(f2))) {
 		stop('the fith variable (f2) must be a factor, you have entered ', class(f2)[1], " data.")
 	}
-	if(!(is.numeric(c1) | is.double(c1) | is.integer(c1))) {
-		stop('the second variable (c1) must be a numeric or dbl or integer, you have entered ', class(c1)[1], " data.")
-	}
-	if(!(is.numeric(c2) | is.double(c2) | is.integer(c2))) {
-		stop('the third variable (c2) must be a numeric or dbl or integer, you have entered ', class(c2)[1], " data.")
-	}
+	# if(!(is.numeric(c1) | is.double(c1) | is.integer(c1))) {
+	# 	stop('the second variable (c1) must be a numeric or dbl or integer, you have entered ', class(c1)[1], " data.")
+	# }
+	# if(!(is.numeric(c2) | is.double(c2) | is.integer(c2))) {
+	# 	stop('the third variable (c2) must be a numeric or dbl or integer, you have entered ', class(c2)[1], " data.")
+	# }
 	if(!(is.character(xlab))) {
 		stop('the x-axis lable (xlab) must be a character, you have entered ', class(xlab)[1], " data.")
 	}
 	if(!(is.character(ylab))) {
 		stop('the y-axis lable (ylab) must be a character, you have entered ', class(ylab)[1], " data.")
 	}
-	ggplot2::ggplot(data = data, ggplot2::aes(c1, c2)) +
+	ggplot2::ggplot(data = data, ggplot2::aes_string(c1, c2)) +
 		ggplot2::geom_point(alpha = 1) +
-		ggplot2::facet_wrap(c(paste(f1), paste(f2))) +
+		ggplot2::facet_wrap(c(f1, f2)) +
 		ggplot2::labs(x = xlab) +
 		ggplot2::labs(y = ylab) +
 		ggplot2::theme_bw(base_size = 10)
